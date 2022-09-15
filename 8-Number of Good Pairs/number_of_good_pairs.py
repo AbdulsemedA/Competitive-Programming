893fb0d9a335dc4c999b62892f0628471374a231
Abdulsemed Abdelila Andya <88716122+AbdulsemedA@users.noreply.github.com>
2022-09-15 18:39:43 +0300

    Create number_of_good_pairs.py

---
+++ 8-Number of Good Pairs/number_of_good_pairs.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        counter = 0
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if nums[i] == nums[j] and i < j:
+                    counter+= 1
+        return counter
