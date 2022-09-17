e7e5a4c58a34baf1d784901d96aab04649ed03bc
Abdulsemed Abdelila Andya <88716122+AbdulsemedA@users.noreply.github.com>
2022-09-17 12:08:35 +0300

    commit

---
+++ 24-Counting Sort 1/counting_sort.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # Write your code here
+    arr.sort()
+    arr2 = []
+    for i in range(100):
+        if i in arr:
+            arr2.append(arr.count(i))
+            arr.remove(i)
+        else:
+            arr2.append(0)
+    return arr2
+         
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
