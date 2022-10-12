72ab78cf7939f8f534e7b3bd551082bb1d461aa7
Abdulsemed Abdelila Andya <88716122+AbdulsemedA@users.noreply.github.com>
2022-10-12 16:46:52 +0300

    Update rotate_array.py

--- 22-Rotate Array/rotate_array.py
+++ 22-Rotate Array/rotate_array.py
@@ -1,15 +1,16 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        k = k % len(nums)
-        l, r =0, len(nums) - 1
-        while l < r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l, r = l + 1, r-1
-        l,r=0,k-1
-        while l < r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l, r = l + 1, r-1
-        l, r =k, len(nums) - 1
-        while l < r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l,r = l + 1,r-1
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k %= len(nums)
+        nums2 = []
+        for i in range(len(nums)):
+            if k > 0:
+                if i >= len(nums) - k:
+                    nums2.append(nums[i])
+        for j in range(len(nums) - k):
+            nums2.append(nums[j])
+        nums.clear()
+        for m in nums2:
+            nums.append(m)
