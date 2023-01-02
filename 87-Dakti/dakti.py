24c54117e51813f9a807ef66f35e98034046e70d
Abdulsemed Abdelila Andya <88716122+AbdulsemedA@users.noreply.github.com>
2023-01-02 19:11:10 +0300

    Create dakti.py

---
+++ 87-Dakti/dakti.py
@@ -0,0 +1,9 @@
+def dak(words):
+    words = words.split()
+    line = sorted(words, key =lambda word: sorted(word))
+    lines = [''.join([char for char in word if not char.isdigit()]) for word in line]
+    print(' '.join(lines))
+size = int(input())
+for _ in range(size):
+    words = input()
+    dak(words)
