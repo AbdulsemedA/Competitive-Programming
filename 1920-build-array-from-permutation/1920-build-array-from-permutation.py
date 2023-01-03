3c39b12507164a96848a989ac3bf1fa492647181
Abdulsemed Abdelila Andya <88716122+AbdulsemedA@users.noreply.github.com>
2023-01-03 14:54:35 +0300

    Delete 1920-build-array-from-permutation directory

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def buildArray(self, nums: List[int]) -> List[int]:
-        size = len(nums)
-        answer = [nums[nums[item]] for item in range(size)]
-        
-        return answer
-        
