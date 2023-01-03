2d1c66707d5216b9876284d882acd0759e79958a
Abdulsemed Abdelila Andya <88716122+AbdulsemedA@users.noreply.github.com>
2023-01-03 16:36:31 +0300

    Create planet.py

---
+++ 93-Planet/planet.py
@@ -0,0 +1,22 @@
+def planet(lists,K):
+    result = 0
+    dictionary = {}
+        
+    for item in lists:
+        dictionary[item] = 1 + dictionary.get(item,0)
+        
+    for item in dictionary:
+        if dictionary[item] >= K:
+            result += K
+        else:
+            result += dictionary[item]
+    
+    print(result)
+    
+
+N = int(input())
+
+for _ in range(N):
+    size, K = map(int,input().split())
+    lists = list(map(int,input().split()))
+    planet(lists,K)
