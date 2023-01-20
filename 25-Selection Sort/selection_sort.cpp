4aef9458155c5f1dbdd332d85f88eaca64a57b49
Abdulsemed Abdelila Andya <88716122+AbdulsemedA@users.noreply.github.com>
2023-01-20 19:15:38 +0300

    Update selection_sort.cpp

--- 25-Selection Sort/selection_sort.cpp
+++ 25-Selection Sort/selection_sort.cpp
@@ -1,74 +1,23 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-void swap(int *xp, int *yp)
-{
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
-
-
-// } Driver Code Ends
-class Solution
-{
-    public:
-    int select(int arr[], int i)
-    {
-        // code here such that selectionSort() sorts arr[]
-    }
-     
-    void selectionSort(int arr[], int n)
-    {
-       //code here
-        int i,j,loc,temp,min;
-        for(i=0;i<n-1;i++){
-            min=arr[i];
-            loc=i;
-            for(j=i+1;j<n;j++){
-                if(min>arr[j]){
-                    min=arr[j];
-                    loc=j;
-                }
-            }
- 
-            temp=arr[i];
-            arr[i]=arr[loc];
-            arr[loc]=temp;
-        }
-    }
-};
-
-//{ Driver Code Starts.
- 
-/* Function to print an array */
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i=0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-}
- 
-// Driver program to test above functions
-int main()
-{
-    int arr[1000],n,T,i;
-  
-    scanf("%d",&T);
+class Solution: 
+    def select(self, arr, i):
+        return i
     
-    while(T--){
+    def selectionSort(self, array,n):
+        for i in range(n):
+            min_idx = self.select(array,i)
+            for j in range(i + 1, n):
+                if array[min_idx] > array[j]:
+                    min_idx = j
+            array[i], array[min_idx] = array[min_idx], array[i]
+            
+        return array
         
-    scanf("%d",&n);
-    
-    for(i=0;i<n;i++)
-      scanf("%d",&arr[i]);
-      
-    Solution ob;  
-    ob.selectionSort(arr, n);
-    printArray(arr, n);
-    }
-    return 0;
-}
-
-// } Driver Code Ends
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        Solution().selectionSort(arr, n)
+        for i in range(n):
+            print(arr[i],end=" ")
+        print()
