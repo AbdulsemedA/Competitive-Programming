aa1c11a9a26019f830a94833108c29a67d568cdc
Abdulsemed Abdelila Andya <88716122+AbdulsemedA@users.noreply.github.com>
2023-02-27 23:06:30 +0300

    Time: 4788 ms (5.03%), Space: 39.1 MB (77.85%) - LeetHub

---
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.py
@@ -0,0 +1,34 @@
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        n = len(s)
+        prefix = [0] * (n+1)
+        
+        for left, right, path in shifts:
+            if path:
+                prefix[left] += 1
+                prefix[right + 1] -= 1
+            else:
+                prefix[left] -= 1
+                prefix[right + 1] += 1
+                
+        curr = 0
+        prefix.pop()
+        
+        for i in range(1, n):
+            curr = prefix[i-1]
+            prefix[i] += curr
+            
+        for i in range(len(prefix)):
+            new = ord(s[i]) + prefix[i]
+            
+            if new < 97:
+                while new < 97:
+                    new += 26
+            elif new > 122:
+                while new > 122:
+                    new -= 26
+            
+            prefix[i] = chr(new)
+        
+        return ''.join(prefix)
+        
