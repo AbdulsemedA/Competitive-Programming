27bcb51c681292677c948a88d2b27601402663cb
Abdulsemed Abdelila Andya <88716122+AbdulsemedA@users.noreply.github.com>
2023-03-01 11:31:46 +0300

    added car-pooling.python

---
+++ car-pooling.py
@@ -0,0 +1,14 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        prefix = [0 for i in range(1002)]
+        
+        for people, start, end in trips:
+            prefix[start] += people
+            prefix[end] -= people
+        
+        curr = 0
+        
+        for i in range(1,len(prefix)):
+            prefix[i] += prefix[i-1]
+        
+        return max(prefix) <= capacity
