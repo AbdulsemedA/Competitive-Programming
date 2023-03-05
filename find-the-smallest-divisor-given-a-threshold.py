26b5725873c3e1a4eec718b38b7a2332820621fb
Abdulsemed Abdelila Andya <88716122+AbdulsemedA@users.noreply.github.com>
2023-03-05 19:59:01 +0300

    added find-the-smallest-divisor-given-a-threshold.python

--- find-the-smallest-divisor-given-a-threshold.py
+++ find-the-smallest-divisor-given-a-threshold.py
@@ -1,17 +1,15 @@
 class Solution:
     def smallestDivisor(self, nums: List[int], threshold: int) -> int:
-        nums.sort()
-        divisors = [x for x in range(1, nums[-1] + 1)]
-        left, right = 0, len(divisors) - 1
+        left, right = 1, max(nums)
 
         while left <= right:
             mid = left + (right - left) // 2
             total = 0
             for num in nums:
-                total += math.ceil(num / divisors[mid])
+                total += math.ceil(num / mid)
             if total > threshold:
                 left = mid + 1
             else:
                 right = mid - 1
                 
-        return divisors[left]
+        return left
