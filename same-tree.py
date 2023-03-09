8d418a37c9490a0672b9252c851da08920bf1302
Abdulsemed Abdelila Andya <88716122+AbdulsemedA@users.noreply.github.com>
2023-03-09 15:58:55 +0300

    added same-tree.python

---
+++ same-tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        def compare(p, q):
+            if not p and not q:
+                return True
+            if (p and not q) or (q and not p):
+                return False
+            if p.val != q.val:
+                return False
+            left = compare(p.left, q.left)
+            right = compare(p.right, q.right)
+
+            return left and right
+        return compare(p, q)
