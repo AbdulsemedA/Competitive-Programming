cdd95d6ec006c901dd3948e1ec49f97a08512044
Abdulsemed Abdelila Andya <88716122+AbdulsemedA@users.noreply.github.com>
2023-04-11 18:05:49 +0300

    added minimum-number-of-vertices-to-reach-all-nodes.python

--- minimum-number-of-vertices-to-reach-all-nodes.py
+++ minimum-number-of-vertices-to-reach-all-nodes.py
@@ -1,15 +1,8 @@
 class Solution:
     def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
-        sources = []
-        src_snk = [[False, False] for _ in range(n)]
+        sink = [False for _ in range(n)]
 
-        for edge in edges:
-            src, des = edge
-            src_snk[src][0] = True
-            src_snk[des][1] = True
+        for src, des in edges:
+            sink[des] = True
 
-        for i in range(len(src_snk)):
-            if src_snk[i][0] and not src_snk[i][1]:
-                sources.append(i)
-        
-        return sources
+        return [i for i in range(len(sink)) if not sink[i]]
